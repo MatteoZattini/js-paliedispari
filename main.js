c32d312232319cf3e41bc554ac729e3998252173
MatteoZattini <matteozattini@hotmail.com>
2024-04-29 22:33:23 +0200

    esercizio palindroma completo

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 // chiedo al'utente una parola tramite prompt
 let inputUser = prompt("inserisci parola")
-console.log(inputUser)
+
 // creo una funzione per controllare se la parola è
 // palindroma
+function controlloParola (parola) {
 
-let carattere;
-let parolaInversa = '';
-let i = inputUser.length - 1
-while(i >= 0) {
-    carattere = inputUser[i];
-    parolaInversa += carattere;
-    i--;
-    console.log(carattere)
-}
-if (inputUser === parolaInversa){
-    console.log("è palindroma")
-}
-else {
-    console.log("non è palindroma")
+ // mi creo una variabile d'appoggio
+let ParolaInversa = ""
+
+// itero la parola carattere per carattere dall'indice più alto
+    for (let i = parola.length - 1; i >= 0; i--) {
+        // sommo carattere per carattere in ordine decrescente dell'index
+        ParolaInversa += parola.charAt(i)
+    }
+
+// controllo se la parola è palindroma o no
+    if ( inputUser == ParolaInversa ) {
+        return "è palindroma"
+    } else {
+        return "non è palindroma"
+    }
 }
 
+console.log(controlloParola(inputUser))
